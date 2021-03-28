54122d57422a2c26a0af4583b805c390bee3fe6c
Felipe Fernandes <nandz.it@gmail.com>
2021-03-28 18:22:55 +0200

    conversation retrieve added

--- message.py
+++ message.py
@@ -3,11 +3,16 @@ from json.decoder import JSONDecodeError
 import pdb
 import os.path
 import os
+from datetime import datetime
 
 test = {"receiver": 'Mario', 'sender': 'Giovanni', 'content': 'Il cane è ubriaco'}
 
-def saveMessage(body):
 
+def saveMessage(body):
+    message = body
+    time = "{}".format(datetime.now())
+    message["at"] = time
+    
     if os.path.isfile('db.json'): 
         checkf = open("db.json")
     else:    
@@ -17,19 +22,33 @@ def saveMessage(body):
         try:
             data=json.load(file)
             temp = data['messages']
-            temp.append(body)
+            temp.append(message)
             file.seek(0)
             json.dump({"messages": temp}, file, indent=2)
             file.truncate()
         except JSONDecodeError: 
             file.seek(0)
-            json.dump({'messages':[ body]}, file, indent=2)    
+            json.dump({'messages':[message]}, file, indent=2)    
             
 saveMessage(test)
 
+def retrieveConversation (receiver, sender):
+    try:
+        messages = []
+        with open("db.json", "r") as file:
+            pdb.set_trace()
+            data=json.load(file)
+            temp = data['messages']
+            for message in temp:
+                if message["receiver"] == receiver and message["sender"] == sender:
+                    messages.append(message)
+                    print(messages) 
+                    print(added)
+            messages.sort(key=lambda x: datetime.datetime.strptime(x['at'], '%d/%m/%y %H:%M:%S'))
+            return messages
+    except:
+        return None
 
 
-#Retrieve message 
-#Retrieve all the messages that has retrieve id and sender id passed as parameters
 
-    
+print(retrieveConversation("Mario", "Giovanni"))
